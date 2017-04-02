81a9284418e89a362acb101fe0c7e732ad9ebe01
HasibeZafer <2012010205052@ogrenci.karabuk.edu.tr>
2017-04-02 19:14:57 +0300

    Added Strings files

---
+++ strings.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+str = 'Hello World!'
+
+print (str)          # Stringin tamamını yazar
+print (str[0])       # Stringin ilk karakterini yazar
+print (str[1:4])     # Stringin 2. karakterinden 4. karakterine kadar olan kısmı yazar.(2-3-4)
+print (str[2:])      # 3. karakterden başlayarak stringin tamamını yazar
+print (str * 2)      # Stringi iki kere yazdırır
+print (str + "Test") # Birleştirilmiş stringi yazar
